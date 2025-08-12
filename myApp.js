9fc5f1d4665c63568a1822b7f8e79a2e7d01d0d2
beingzenitsu <ojasvchaudhary0@gmail.com>
2025-08-12 15:14:15 +0530

    .env changes

--- myApp.js
+++ myApp.js
@@ -12,14 +12,13 @@ app.get('/', (req, res) => {
 app.use('/public',express.static(__dirname + '/public'));
 
 app.get('/json', (req, res) => { 
-  let message = 'Hello json'
-
-  if (process.env.MESSAGE_STYLE === 'uppercase'){
-      message = message.toUpperCase();
-  }
   
-  res.json({'message': message});
-});
+  if (process.env.MESSAGE_STYLE === 'uppercase'){
+      res.json({'message': "HELLO JSON"});
+  } else {
+    res.json({message: "hello json"})
+  }
+    });
 
 
 
